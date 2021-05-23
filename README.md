2bf014e10822160267d1b93ba92697ac80617aee
jr2nd <junior2nd@gmail.com>
2021-05-23 07:14:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# calcyoulater-ymhmxm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/calcyoulater-ymhmxm)
---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+let buttons = document.querySelectorAll('.button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', clickButton);
+} //for
+
+function clickButton() {
+  let symbol = this.innerHTML;
+  let entry = document.getElementById('entry');
+  let result = document.getElementById('result');
+  let operation = document.getElementById("operation");
+if(Number.isInteger(parseInt(symbol))){
+      entry.innerHTML = parseInt(entry.innerHTML) * 10 + parseInt(symbol);
+return;
+}//if
+        switch (operation.innerHTML) {
+          case '?':
+operation.innerHTML = symbol;
+result.innerHTML = entry.innerHTML;
+entry.innerHTML = 0;
+          break;
+    case '+':
+      result.innerHTML =
+        parseFloat(entry.innerHTML) + parseFloat(result.innerHTML);
+      break;
+    case '-':
+       result.innerHTML =
+        parseFloat(result.innerHTML) - parseFloat(entry.innerHTML);
+      break;
+    case '/':
+      if (parseFloat(entry.innerHTML) == 0) return;
+      result.innerHTML = parseFloat(result.innerHTML) / parseFloat(entry.innerHTML);
+      break;
+    case '*':
+result.innerHTML = result.innerHTML * entry.innerHTML;
+      break;
+    case '.':
+      if (entry % 10) return;
+      entry.innerHTML = entry.innerHTML + '.';
+      break;
+      case "clear<br>all":
+      result.innerHTML = '0';
+      entry.innerHTML = '0';
+      break;
+    case '&#8626;': //Enter
+      entry = 0;
+      result = 0.0;
+      break;
+    default:
+      return;
+  } //switch
+  entry.innerHTML = 0;
+  operation.innerHTML = symbol;
+} //clickButton()
